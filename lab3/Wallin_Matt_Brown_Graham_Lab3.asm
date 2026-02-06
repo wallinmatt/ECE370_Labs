eebf009f9ad6b43716110210b71631f596c447a3
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2026-02-05 19:58:46 -0800

    add scrolling capabilties

--- lab3/Wallin_Matt_Brown_Graham_Lab3.asm
+++ lab3/Wallin_Matt_Brown_Graham_Lab3.asm
@@ -13,11 +13,15 @@
 ;***********************************************************
 .def	mpr = r16				; Multipurpose register is required for LCD Driver
 .def	temp = r17
+.def	waitcnt = r18
+.def	ilcnt = r19
+.def	olcnt = r23
 
 .equ	buffTime = 25
 .equ	button4 = 4
 .equ	button5 = 5
 .equ	button7 = 7
+.equ	WTime = 25
 
 ;***********************************************************
 ;*	Start of Code Segment
@@ -68,20 +72,20 @@ MAIN:
 
 		in		mpr, PIND
 		andi	mpr, (1<<button4|1<<button5|1<<button7)
-		cpi   mpr, ~(1<<button5) & ((1<<button4|1<<button5|1<<button7))
+		cpi   mpr, (1<<button4|1<<button7)
 		brne CHECK_B4
 		rcall storeString1
 		rcall storeString2
 		rcall LCDWrite
 		rjmp	MAIN
 CHECK_B4:	
-		cpi   mpr, ~(1<<button4) & ((1<<button4|1<<button5|1<<button7))
+		cpi   mpr, (1<<button5|1<<button7)
 		brne CHECK_B7
 		rcall LCDClr
 		rcall LCDWrite
 		rjmp MAIN
 CHECK_B7: 
-		cpi   mpr, ~(1<<button7) & ((1<<button4|1<<button5|1<<button7))
+		cpi   mpr, (1<<button4|1<<button5)
 		brne MAIN
 		rcall storeString1
 		rcall storeString2
@@ -177,41 +181,75 @@ storeString2:
 		ret						; End a function with RET
 
 displayTicker: 
+    push ZL
+    push ZH
+    push YL
+    push YH
+    push mpr
+    push temp
 
-	push ZL
-	push ZH
-	push YL
-	push YH
-	push XL
-	push XH
+TICKER:
+    ; Save the first character
+    ldi YL, low($0100)
+    ldi YH, high($0100)
+    ld temp, Y              ; Save first char in temp
 
-	ldi XL, low($011F)
-	ldi XH, high($011F)
-	ldi YL, low($0100)
-	ldi YH, high($0100)
-	ldi ZL, low ($0101)
-	ldi ZH, high($0101)
-	ld mpr, X
-	LOOP3:
-		ld temp, Z
-		st Y+, mpr 
-		mov temp, mpr
-		cpi YL, low(STRING2_END<<1)
-		brne LOOP3
-		cpi YH, high(STRING2_END<<1)
-		brne LOOP3
-	rcall LCDWrite
+    ldi ZL, low($0101)      ; Z = source (starts at second char)
+    ldi ZH, high($0101)
+    
+SHIFT_STRS:
+    ld mpr, Z+              ; Load from source, increment Z
+    st Y+, mpr              ; Store to dest, increment Y
+    cpi YL, low($0120)      ; Check if we've reached end of buffer
+    brne SHIFT_STRS
+    cpi YH, high($0120)
+    brne SHIFT_STRS
 
-	pop XH
-	pop XL
-	pop YH
-	pop YL
-	pop ZH
-	pop ZL
-	ret
+    ; Put the saved first character at the end
+    ldi YL, low($011F)
+    ldi YH, high($011F)
+    st Y, temp
+
+    rcall LCDWrite
+	ldi		waitcnt, WTime
+	rcall Wait
+	rjmp TICKER
+
+    pop temp
+    pop mpr
+    pop YH
+    pop YL
+    pop ZH
+    pop ZL
+    ret
 		
 
+;----------------------------------------------------------------
+; Sub:	Wait
+; Desc:	A wait loop that is 16 + 159975*waitcnt cycles or roughly
+;		waitcnt*10ms.  Just initialize wait for the specific amount
+;		of time in 10ms intervals. Here is the general eqaution
+;		for the number of clock cycles in the wait loop:
+;			(((((3*ilcnt)-1+4)*olcnt)-1+4)*waitcnt)-1+16
+;----------------------------------------------------------------
+Wait:
+		push	waitcnt			; Save wait register
+		push	ilcnt			; Save ilcnt register
+		push	olcnt			; Save olcnt register
 
+Loop:	ldi		olcnt, 224		; load olcnt register
+OLoop:	ldi		ilcnt, 237		; load ilcnt register
+ILoop:	dec		ilcnt			; decrement ilcnt
+		brne	ILoop			; Continue Inner Loop
+		dec		olcnt		; decrement olcnt
+		brne	OLoop			; Continue Outer Loop
+		dec		waitcnt		; Decrement wait
+		brne	Loop			; Continue Wait loop
+
+		pop		olcnt		; Restore olcnt register
+		pop		ilcnt		; Restore ilcnt register
+		pop		waitcnt		; Restore wait register
+		ret				; Return from subroutine
 
 ;***********************************************************
 ;*	Stored Program Data
@@ -222,11 +260,11 @@ displayTicker:
 ; after the .DB directive; these can help to access the data
 ;-----------------------------------------------------------
 STRING1_BEG:
-.DB		"Batt and Graham "		; Declaring data in ProgMem
+.DB		"Matt and Graham "		; Declaring data in ProgMem
 STRING1_END:
 
 STRING2_BEG:
-.DB		"were here!"		; Declaring data in ProgMem
+.DB		"were here!      "		; Declaring data in ProgMem
 STRING2_END:
 
 ;***********************************************************
