3f744f238ec6e09a801f5252180446d39302f41a
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2026-02-19 13:52:35 -0800

    add HitLeft and HitRight functions and interupt vectors

--- lab5/Wallin_Matt_Brown_Graham_Lab5.asm
+++ lab5/Wallin_Matt_Brown_Graham_Lab5.asm
@@ -1,8 +1,8 @@
 ;***********************************************************
 ;*	This is the skeleton file for Lab 5 of ECE 375
 ;*
-;*	 Author: Enter your name
-;*	   Date: Enter Date
+;*	 Author: Graham Brown and Matt Wallin
+;*	   Date: 2/20/2026
 ;*
 ;***********************************************************
 
@@ -33,6 +33,13 @@
 ;.org	$002E					; Analog Comparator IV
 ;		rcall	HandleAC		; Call function to handle interrupt
 ;		reti					; Return from interrupt
+.org	$0002
+		rcall	HitRight
+		reti
+
+.org	$0004
+		rcall	HitLeft
+		reti
 
 .org	$0056					; End of Interrupt Vectors
 
@@ -41,10 +48,26 @@
 ;***********************************************************
 INIT:							; The initialization routine
 		; Initialize Stack Pointer
+		ldi		mpr, low(RAMEND)
+		out		SPL, mpr		; Load SPL with low byte of RAMEND
+		ldi		mpr, high(RAMEND)
+		out		SPH, mpr		; Load SPH with high byte of RAMEND
+
+		clr		zero			; Set the zero register to zero, maintain
+										; these semantics, meaning, dont
+										; load anything else into it.
 
 		; Initialize Port B for output
+		ldi		mpr, $FF		; Set Port B Data Direction Register
+		out		DDRB, mpr		; for output
+		ldi		mpr, $00		; Initialize Port B Data Register
+		out		PORTB, mpr		; so all Port B outputs are low
 
 		; Initialize Port D for input
+		ldi		mpr, $00		; Set Port D Data Direction Register
+		out		DDRD, mpr		; for input
+		ldi		mpr, $FF		; Initialize Port D Data Register
+		out		PORTD, mpr		; so all Port D inputs are Tri-State
 
 		; Initialize external interrupts
 			; Set the Interrupt Sense Control to falling edge
@@ -74,6 +97,74 @@ MAIN:							; The Main program
 ;	interrupt, and maybe a wait function
 ;------------------------------------------------------------
 
+;----------------------------------------------------------------
+; Sub:	HitRight
+; Desc:	Handles functionality of the TekBot when the right whisker
+;		is triggered.
+;----------------------------------------------------------------
+HitRight:
+		push	mpr			; Save mpr register
+		push	waitcnt			; Save wait register
+		in		mpr, SREG	; Save program state
+		push	mpr			;
+
+		; Move Backwards for a second
+		ldi		mpr, MovBck	; Load Move Backward command
+		out		PORTB, mpr	; Send command to port
+		ldi		waitcnt, WTime*2	; Wait for 2 seconds
+		rcall	Wait			; Call wait function
+
+		; Turn left for a second
+		ldi		mpr, TurnL	; Load Turn Left Command
+		out		PORTB, mpr	; Send command to port
+		ldi		waitcnt, WTime	; Wait for 1 second
+		rcall	Wait			; Call wait function
+
+		; Move Forward again
+		ldi		mpr, MovFwd	; Load Move Forward command
+		out		PORTB, mpr	; Send command to port
+
+		pop		mpr		; Restore program state
+		out		SREG, mpr	;
+		pop		waitcnt		; Restore wait register
+		pop		mpr		; Restore mpr
+		ret				; Return from subroutine
+
+;----------------------------------------------------------------
+; Sub:	HitLeft
+; Desc:	Handles functionality of the TekBot when the left whisker
+;		is triggered.
+;----------------------------------------------------------------
+HitLeft:
+		push	mpr			; Save mpr register
+		push	waitcnt			; Save wait register
+		in		mpr, SREG	; Save program state
+		push	mpr			;
+
+		; Move Backwards for a second
+		ldi		mpr, MovBck	; Load Move Backward command
+		out		PORTB, mpr	; Send command to port
+		ldi		waitcnt, WTime*2	; Wait for 2 seconds
+		rcall	Wait			; Call wait function
+
+		; Turn right for a second
+		ldi		mpr, TurnR	; Load Turn Left Command
+		out		PORTB, mpr	; Send command to port
+		ldi		waitcnt, WTime	; Wait for 1 second
+		rcall	Wait			; Call wait function
+
+		; Move Forward again
+		ldi		mpr, MovFwd	; Load Move Forward command
+		out		PORTB, mpr	; Send command to port
+
+		pop		mpr		; Restore program state
+		out		SREG, mpr	;
+		pop		waitcnt		; Restore wait register
+		pop		mpr		; Restore mpr
+		ret				; Return from subroutine
+
+
+
 ;-----------------------------------------------------------
 ; Func: Template function header
 ; Desc: Cut and paste this and fill in the info at the
@@ -99,3 +190,4 @@ FUNC:							; Begin a function with a label
 ;*	Additional Program Includes
 ;***********************************************************
 ; There are no additional file includes for this program
+
