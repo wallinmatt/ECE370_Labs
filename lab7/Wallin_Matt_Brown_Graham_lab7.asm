11e0a4ffb0547eb071e892cbe23fd92d3f76e28d
Matt Wallin <wallinm@oregonstate.edu>
2026-03-12 11:46:23 -0700

    Lab7 skeleton code

---
+++ lab7/Wallin_Matt_Brown_Graham_lab7.asm
@@ -0,0 +1,87 @@
+
+;***********************************************************
+;*
+;*	This is the TRANSMIT skeleton file for Lab 7 of ECE 375
+;*
+;*  	Rock Paper Scissors
+;* 	Requirement:
+;* 	1. USART1 communication
+;* 	2. Timer/counter1 Normal mode to create a 1.5-sec delay
+;***********************************************************
+;*
+;*	 Author: Enter your name
+;*	   Date: Enter Date
+;*
+;***********************************************************
+
+.include "m32U4def.inc"         ; Include definition file
+
+;***********************************************************
+;*  Internal Register Definitions and Constants
+;***********************************************************
+.def    mpr = r16               ; Multi-Purpose Register
+
+; Use this signal code between two boards for their game ready
+.equ    SendReady = 0b11111111
+
+;***********************************************************
+;*  Start of Code Segment
+;***********************************************************
+.cseg                           ; Beginning of code segment
+
+;***********************************************************
+;*  Interrupt Vectors
+;***********************************************************
+.org    $0000                   ; Beginning of IVs
+	    rjmp    INIT            	; Reset interrupt
+
+
+.org    $0056                   ; End of Interrupt Vectors
+
+;***********************************************************
+;*  Program Initialization
+;***********************************************************
+INIT:
+	;Stack Pointer (VERY IMPORTANT!!!!)
+	;I/O Ports
+	;USART1
+		;Set baudrate at 2400bps
+		;Enable receiver and transmitter
+		;Set frame format: 8 data bits, 2 stop bits
+
+	;TIMER/COUNTER1
+		;Set Normal mode
+
+	;Other
+
+
+;***********************************************************
+;*  Main Program
+;***********************************************************
+MAIN:
+
+	;TODO: ???
+
+		rjmp	MAIN
+
+;***********************************************************
+;*	Functions and Subroutines
+;***********************************************************
+
+;***********************************************************
+;*	Stored Program Data
+;***********************************************************
+
+;-----------------------------------------------------------
+; An example of storing a string. Note the labels before and
+; after the .DB directive; these can help to access the data
+;-----------------------------------------------------------
+STRING_START:
+    .DB		"Welcome!"		; Declaring data in ProgMem
+STRING_END:
+
+;***********************************************************
+;*	Additional Program Includes
+;***********************************************************
+.include "LCDDriver.asm"		; Include the LCD Driver
+
